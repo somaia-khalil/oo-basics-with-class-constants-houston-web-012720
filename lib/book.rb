31ab6aa032adfecb77a26324beadcd5f9b39fe89
somaia-khalil <sumaya_khair@live.com>
2020-01-28 17:17:29 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,13 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
+  attr_accessor :author, :page_count
+  attr_reader :title , :genre
+  GENRES =[]
   def initialize(title)
     @title = title
+  end
+  def genre= (type)
+    @genre = type
+    GENRES << type
   end
 
   def turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,20 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+  end
+  
+  BRANDS = []
+  def initialize(brand)
+    @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
