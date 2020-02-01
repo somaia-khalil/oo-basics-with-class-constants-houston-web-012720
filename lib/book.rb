d0f7115188831224b13493c6ecd8abfe7a12611b
Somaia Khalil <sumaya_khair@live.com>
2020-02-01 18:11:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,17 @@
-# class Book
-#   attr_accessor :author, :page_count
-#   attr_reader :title , :genre
-#   GENRES =[]
-#   def initialize(title)
-#     @title = title
-#   end
-#   def genre= (type)
-#     @genre = type
-#     GENRES << type
-#   end
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title , :genre
+  GENRES =[]
+  def initialize(title)
+    @title = title
+  end
+  def genre= (type)
+    @genre = type
+    GENRES << type
+  end
 
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
-# end
+end
